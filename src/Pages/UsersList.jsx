d38e612b2a104af6bc50577b39e0f15c26a19cd8
Alexander Celis <alexcelis019@gmail.com>
2022-11-11 00:07:52 -0500

    first fetch

--- src/Pages/UsersList.jsx
+++ src/Pages/UsersList.jsx
@@ -1,10 +1,23 @@
 import { FlatList, StyleSheet, View } from 'react-native'
-import users from '../data/users'
+// import users from '../data/users'
 
 /* components */
 import UserItem from '../components/User'
+import { useEffect, useState } from 'react'
 
 export default function UsersList () {
+  const [users, setUsers] = useState(null)
+
+  const fetchData = async () => {
+    const response = await globalThis.fetch('https://dummyjson.com/users?limit=20')
+    const { users } = await response.json()
+    setUsers(users)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
     <View style={styles.container}>
       <FlatList
